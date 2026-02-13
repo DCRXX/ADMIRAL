e1adb4524a682694d3bc849a6bdcfa659b4e6bea
DCRXX <linnik654g@gmail.com>
2026-02-13 08:34:24 +0300

    2-block-comlete

--- admiral/src/main.jsx
+++ admiral/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import  Announcements from './components/Announcements/Announcements'
+import CoachingStaff from './components/CoachingStaff/CoachingStaff'
 import './index.css'
-import App from './App.jsx'
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <>
+        <Announcements/>
+        <CoachingStaff/>
+    </>
 )
