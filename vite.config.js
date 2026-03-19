e865da81f24716bfbbfc2f84715913438f5fda2b
DCRXX <linnik654g@gmail.com>
2026-03-19 19:44:39 +0300

    update config

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,9 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', 
+  base: '.', 
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
     emptyOutDir: true,
   },
   resolve: {
